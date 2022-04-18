45a98dcdea4b090f837a2954a5d4d1385311ae5f
Rajesh Indukuri <info9rajeshcs@gmail.com>
2022-04-17 22:36:01 -0700

    updated

--- script.js
+++ script.js
@@ -1,37 +1,73 @@
-let cursor = document.querySelector('.cursor');
+$(document).ready(function(){
+    $(window).scroll(function(){
+        // sticky navbar on scroll script
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+        
+        // scroll-up button show/hide script
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
 
-gsap.set(cursor, {
-  xPercent: -50,
-  yPercent: -50
+    // slide-up script
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+        // removing smooth scroll on slide-up button click
+        $('html').css("scrollBehavior", "auto");
+    });
+
+    $('.navbar .menu li a').click(function(){
+        // applying again smooth scroll on menu items click
+        $('html').css("scrollBehavior", "smooth");
+    });
+
+    // toggle menu/navbar script
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+
+    // typing text animation script
+    var typed = new Typed(".typing", {
+        strings: ["Developer", "Blogger", "Designer", "Freelancer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    var typed = new Typed(".typing-2", {
+        strings: ["Developer", "Blogger", "Designer", "Freelancer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    // owl carousel script
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplay: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0:{
+                items: 1,
+                nav: false
+            },
+            600:{
+                items: 2,
+                nav: false
+            },
+            1000:{
+                items: 3,
+                nav: false
+            }
+        }
+    });
 });
-
-document.addEventListener('mousemove', e =>{
-    // cursor.setAttribute('style', 'top:'+(e.pageY - 10)+'px; left:'+(e.pageX - 10)+ "px;")
-  
-  gsap.set(cursor, {
-    x: e.x,
-    y: e.y
-  });
-})
-
-let liReseau = document.querySelectorAll('.icone')
-tableauReseau = Array.from(liReseau)
-
-let test = tableauReseau
-
-tableauReseau.forEach(element => {
-    element.addEventListener('mouseover', function() {
-        gsap.to(cursor, {
-            width:40,
-            height:40,
-            backgroundColor: 'transparent'
-        })
-    })
-    element.addEventListener('mouseout', function() {
-        gsap.to(cursor, {
-            width:20,
-            height:20,
-            backgroundColor: 'black'
-        })
-    })  
-})
